b1d3b88fccae254d2ab9c17e2b05e07ed91fdad1
Mankarn Dosanjh <mankarndosanjh114@gmail.com>
2025-01-20 14:05:29 +0000

    Comments added. Code finalised. Keeping test data so users can see how it works.

--- brute_force_decrypter.py
+++ brute_force_decrypter.py
@@ -1,42 +1,36 @@
 # Module imports.
 from pathlib import Path
-from pypdf import PdfReader, errors
-
-# Plan
-# Iterate through PDF files with globby glob glob.
-# Skip decrypted files.
-# Iterate through dictionary.txt
-# Attempt decryption using upper and lowercase version of each word.
-# Print out password if decryption is successful and move onto next file.
+from pypdf import PdfReader
 
 # Prompts user for folder and validates input.
 pdf = Path(input("PDF file to brute force:\n"))
-
 if not pdf.is_file() and pdf.suffix != '.pdf':
     print("ERROR - INVALID DIRECTORY!")
     quit()
 
 # Prompts user for password text file and validates input.
 text_file = Path(input("\nText file storing password attempts:\n"))
-
 if not text_file.is_file() and text_file.suffix != '.txt':
     print("ERROR - VALID TEXT FILE NOT PROVIDED!")
     quit()
 
+# Variable initilisation.
 reader = PdfReader(pdf)
 count = 0
-
 with open(text_file) as fhandle:
     passwords = fhandle.readlines()
 
-# Thing other thing
 if reader.is_encrypted:
     for word in passwords:
         count += 1
+
+        # Attempts decryption with word variations.
         if reader.decrypt(word.lower().strip()):
-            print(f'\r\n\n{pdf} CRACKED\n\nPASSWORD: {word.lower().strip()}')
+            print(f'\n\n{pdf} CRACKED\n\nPASSWORD: {word.lower().strip()}')
             break
         elif reader.decrypt(word.upper().strip()):
-            print(f'\r\n\n{pdf} CRACKED\n\nPASSWORD: {word.upper().strip()}')
+            print(f'\n\n{pdf} CRACKED\n\nPASSWORD: {word.upper().strip()}')
             break
+
+        # Tracks progress of password attempts.
         print(f'\r{round((count/len(passwords)) * 100, 2)}%', flush=True, end="")
--- test.py
+++
@@ -1,7 +0,0 @@
-from time import sleep
-
-for i in range(21):
-    spaces = " " * (20 - i)
-    percentage = 5*i
-    print(f"\r[{'='*i}{spaces}]{percentage}%", flush=True, end="")
-    sleep(0.25)
